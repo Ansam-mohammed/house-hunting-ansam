810cd39574042abc642c9228d4f47113b0602905
Ansam-mohammed <ansamm.9099@gmail.com>
2023-08-20 15:52:40 +0300

    add api

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import {Layout, Landing} from '../Pages';
-import NavBar from '../Components/NavBar'
-import logo from '../img/logo.png.png'
+import React  from 'react';
+import {Layout, Landing,} from '../Pages';
+
+
+
 
 
 function App() {
   return (
     <Layout className="App">
     <Landing/>
-  
     </Layout>
+
 
   );
 }
--- src/Components/CardContainer/index.js
+++ src/Components/CardContainer/index.js
@@ -1,14 +1,21 @@
 import React from 'react'; 
 import Card from '../Cards'
-
+import { Grid } from "@mui/material";
+import HouseCard from "../Cards";
 
 
 const CardContainer = ({houses})=> {
 return <>
 {
-    houses.length && houses.map((house)=> <Card house={house} />)
+    <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+    {houses.map((house, index) => (
+      <Grid item xs={2} sm={4} md={4} key={index}>
+        <HouseCard house={house} />
+      </Grid>
+    ))}
+  </Grid>
+   
 }
-
 </>
 
 }
